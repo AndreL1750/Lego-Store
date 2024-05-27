e5bd97006bd779a7ee6854e84a057dbcb257051f
André Lourenço <andrelourencoo1750@gmail.com>
2024-05-27 17:40:47 +0100

    implementado menu de navegação basico

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import Header from './components/Header';
+import Cart from './components/Cart';
+import Profile from './components/Profile';
+
 
 function App() {
+  
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+export default function Navbar() {
+    
+    const [display, setDisplay] = useState(false);
+
+    console.log(display)
+
+    function handleDisplay() {
+        if(!display) {
+            setDisplay(true);
+        }
+        else {
+            setDisplay(false);
+        }
+    }
+    const menu = (display) => {
+
+        if(display) {
+            return (
+                <div className="HambMenu">
+                    <div className="first-btn">
+                        <ul>
+                            <li><button type="button">New Sets</button></li>
+                            <li><button type="button">Sets By Theme</button></li>
+                            <li><button type="button">Best Sellers</button></li>
+                        </ul>
+                    </div>
+                </div>
+            );
+        }
+    }
+
+    return (
+        <nav className="Navbar">
+            <div>
+                <NavLink to="/"><img src="images/lego-logo.svg" alt="" /></NavLink>
+            </div>
+            <div className="first-btn-out">
+                <button className="btn-menu-out" type="button" onClick={() => handleDisplay()}>explore</button>       
+                {menu(display)}
+            </div>        
+        </nav>
+    );
+}
