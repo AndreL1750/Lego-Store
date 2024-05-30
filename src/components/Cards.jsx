94a3a06fa2e880e6187376001b184aa5b6367855
André Lourenço <andrelourencoo1750@gmail.com>
2024-05-30 18:48:14 +0100

    Adicionado componente para fix de commit anterior

---
+++ src/components/Cards.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+export default function Cards() {
+    
+    let [prodMax, setProdMax] = useState(35);
+
+    const [products, setProducts] = useState([]);
+    
+    useEffect(() => {
+             
+        fetch('/legosetshmm.json')
+        .then(response => response.json())
+        .then(product => setProducts(product)); 
+        
+    }, []);
+    
+    console.log(products);
+    console.log(prodMax);
+
+    return (
+        <div>
+            <div className="product-list">
+                {
+                    products.slice(0, prodMax).map((product, index) => (
+                        <div className="product" key={index}>
+                            <img src="/images/lego-logo.svg" alt="" />
+                            <h3>{product.set_name}</h3>
+                            <div>{product.list_price + "$"} {product.star_rating}</div>
+                        </div>
+                    ))
+                }
+            </div>
+            <div className="more-btn-div">
+                <button type="button" onClick={() => setProdMax(prodMax + 35)}>more</button>
+            </div>
+            
+        </div>
+    );
+}
