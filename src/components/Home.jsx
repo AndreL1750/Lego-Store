9ecc5662a14f53cee685124f6cc72ae2f31e7c93
André Lourenço <andrelourencoo1750@gmail.com>
2024-06-11 21:28:01 +0100

    Adicionado conteudo ao menu de navegação e devida estilização, iniciado layout da homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,35 +1,76 @@
-import { NavLink } from "react-router-dom";
-import Cards from "./Cards";
-import Filters from "./Filters";
 
 export default function Home() {
 
     return (
         <main className="Home">
-            <a href="/product">Page Product</a>
-            <div className="filtering">
-                <div>A mostrar X produto(s)</div>
-                <div className="filter-div">
-                    Filter:
-                    <select className="filter">
-                        <option>Recomendados</option>
-                        <option>Preço: Mais baixo a mais alto</option>
-                        <option>Preço: Mais alto a mais baixo</option>
-                        <option>Classificação</option>
-                    </select>
+           <div className="Banner">
+                <img src="/images/Home-Banner.webp" alt="" />
+                <div className="Banner-Content">
+                    <h2>Compra os nossos conjuntos mais recentes</h2>
+                    <p>Vê os lançamentos mais recentes deste mês, como o novo conjunto Rádio Retro.</p>
+                    <div>
+                        <a href="/">
+                            Comprar Agora
+                            <svg width="18" height="28" viewBox="0 0 18 28" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"><path d="M1.825 28L18 14 1.825 0 0 1.715 14.196 14 0 26.285z" fill="currentColor"/></svg>
+                        </a>
+                    </div>
                 </div>
-            </div>
-            <section>
-                <Filters />
-                <div className="content">
-                    <Cards />
+           </div>
+           <div className="section2">
+                <div className="category-btns">
+                    <a href="/">
+                        <img src="/images/New.webp" alt="" />
+                        <h3>Novidades</h3>
+                    </a>
+                    <a href="/">
+                        <img src="/images/Exclusive.webp" alt="" />
+                        <h3>Exclusivos</h3>
+                    </a>
+                    <a href="/">
+                        <img src="/images/Offers.webp" alt="" />
+                        <h3>Ofertas</h3>
+                    </a>
+                    <a href="/">
+                        <img src="/images/Ideas.webp" alt="" />
+                        <h3>Ideias</h3>
+                    </a>
+                    <a href="/">
+                        <img src="/images/Icons.webp" alt="" />
+                        <h3>LEGO® Icons</h3>
+                    </a>
+                    <a href="/">
+                        <img src="/images/Disney.webp" alt="" />
+                        <h3>Disney</h3>
+                    </a>
+                    <a href="/">
+                        <img src="/images/GiftCards.webp" alt="" />
+                        <h3>Cartões de oferta</h3>
+                    </a>
+                    <a href="/">
+                        <img src="/images/Insiders.webp" alt="" />
+                        <h3>LEGO® Insiders</h3>
+                    </a>
                 </div>
-            </section>
-            <div className="next-prev-page">
-                <NavLink to="/product" className="chevron"><img src="/images/chevron.svg" alt="" /></NavLink>
-                <NavLink to="/product">1</NavLink>
-                <NavLink to="/product/2">2</NavLink>
-                <NavLink to="/product/2"><img src="/images/chevron.svg" alt="" /></NavLink>
+           </div>
+           <div className="section3">
+                <div className="video-banner">
+                    <div className="video-banner-a">
+                        <h3>Descobre as inúmeras formas de explorar o espaço</h3>
+                        <a href="/">Explorar
+                            <svg width="18" height="28" viewBox="0 0 18 28" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"><path d="M1.825 28L18 14 1.825 0 0 1.715 14.196 14 0 26.285z" fill="currentColor"/></svg>
+                        </a>
+                        <a href="/">Comprar agora
+                            <svg width="18" height="28" viewBox="0 0 18 28" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"><path d="M1.825 28L18 14 1.825 0 0 1.715 14.196 14 0 26.285z" fill="currentColor"/></svg>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="/">
+                            <video loop autoPlay muted>
+                                <source src="/videos/Lego-Planets.mp4" type="video/mp4" />
+                            </video>
+                        </a>
+                    </div>
+                </div>
             </div>
         </main>
     );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
@@ -24,8 +24,68 @@ export default function Navbar() {
     const borderDisplay1 = !border1 ? "none" : "2px solid #000" ;
     const borderDisplay2 = !border2 ? "none" : "2px solid #000" ;
     const borderDisplay3 = !border3 ? "none" : "2px solid #000" ;
+
+    // Content1
+    const [themeStatus, setThemeStatus] = useState(false);
+    const [ageStatus, setAgeStatus] = useState(false);
+    const [priceStatus, setPriceStatus] = useState(false);
+    const [prodStatus, setProdStatus] = useState(false);
+    const [intStatus, setIntStatus] = useState(false);
+    const [pabStatus, setPabStatus] = useState(false);
+
     
     const menuDisplay = !menuStatus ? "none" : "block";
+    const themeDisplay = !themeStatus ? "none" : "block";
+    const ageDisplay = !ageStatus ? "none" : "block"; 
+    const priceDisplay = !priceStatus ? "none" : "block";
+    const prodDisplay = !prodStatus ? "none" : "block";
+    const intDisplay = !intStatus ? "none" : "block";
+    const pabDisplay = !pabStatus ? "none" : "block";
+
+    
+    const handleThemeBtn = () => {setThemeStatus(!themeStatus); setAgeStatus(false); setPriceStatus(false); setProdStatus(false); setIntStatus(false); setPabStatus(false);};
+    const handleAgeBtn = () => {setAgeStatus(!ageStatus); setThemeStatus(false); setPriceStatus(false); setProdStatus(false); setIntStatus(false); setPabStatus(false);};
+    const handlePriceBtn = () => {setPriceStatus(!priceStatus); setThemeStatus(false); setAgeStatus(false); setProdStatus(false); setIntStatus(false); setPabStatus(false);};
+    const handleProdBtn = () => {setProdStatus(!prodStatus); setThemeStatus(false); setAgeStatus(false); setPriceStatus(false); setIntStatus(false); setPabStatus(false);};
+    const handleIntBtn = () => {setIntStatus(!intStatus); setThemeStatus(false); setAgeStatus(false); setPriceStatus(false); setProdStatus(false); setPabStatus(false);};
+    const handlePabBtn = () => {setPabStatus(!pabStatus); setThemeStatus(false); setAgeStatus(false); setPriceStatus(false); setProdStatus(false); setIntStatus(false);};
+    
+    // Content2
+    const [valStatus, setValStatus] = useState(false);
+    const [appStatus, setAppStatus] = useState(false);
+    const [magStatus, setMagStatus] = useState(false);
+
+    const handleValBtn = () => {setValStatus(!valStatus); setAppStatus(false); setMagStatus(false);};
+    const handleAppBtn = () => {setAppStatus(!appStatus); setValStatus(false); setMagStatus(false);};
+    const handleMagBtn = () => {setMagStatus(!magStatus); setValStatus(false); setAppStatus(false);};
+
+    const valDisplay = !valStatus ? "none" : "block";
+    const appDisplay = !appStatus ? "none" : "block";
+    const magDisplay = !magStatus ? "none" : "block";
+
+    const [contentMaxWidth, setContentMaxWidth] = useState("");
+
+    useEffect(() => {
+        if(themeStatus) {
+            setContentMaxWidth("1500px");
+        }
+        else if(intStatus) {
+            setContentMaxWidth("1300px");
+        }
+        else {
+            setContentMaxWidth("1200px");
+        }
+    }, [themeStatus, intStatus]);
+
+    function menuClose() {
+        setDisplayContent1(false); 
+        setDisplayContent2(false); 
+        setDisplayContent3(false); 
+        setBorder1(false); 
+        setBorder2(false); 
+        setBorder3(false); 
+        setMenuStatus(false);
+    }
 
     return (
         <nav className="Navbar">
@@ -39,12 +99,129 @@ export default function Navbar() {
                     <button style={{borderBottom: borderDisplay3}} className="menu-btn" type="button" onClick={handleDisplayContent3}>Ajuda</button>       
                     <div style={{display: content1}} className="menu-list">
                         <ul>
-                            <li><button type="button">Conjuntos por tema<img src="/images/chevron.svg" alt="" /></button></li>
-                            <li><button type="button">Idade<img src="/images/chevron.svg" alt="" /></button></li>
-                            <li><button type="button">Gama de preços<img src="/images/chevron.svg" alt="" /></button></li>
-                            <li><button type="button">Produtos LEGO<img src="/images/chevron.svg" alt="" /></button></li>
-                            <li><button type="button">Interesses<img src="/images/chevron.svg" alt="" /></button></li>
-                            <li><button type="button">Pick and Build<img src="/images/chevron.svg" alt="" /></button></li>
+                            <li><button type="button"onClick={handleThemeBtn}>Conjuntos por tema<img src="/images/chevron.svg" alt="" /></button></li>
+                            <li style={{display: themeDisplay}}><div className="grid-list styleAnch">
+                                    <div className="grid-list-top-text"><a href="/">Ver todos os temas</a></div>
+                                    <ul>
+                                        <li><a href="/">Architecture</a></li>
+                                        <li><a href="/">Ideas</a></li>
+                                        <li><a href="/">Marvel</a></li>
+                                        <li><a href="/">Batman™</a></li>
+                                        <li><a href="/">Jurassic World</a></li>
+                                        <li><a href="/">Minecraft®</a></li>
+                                        <li><a href="/">A Coleção Botânica</a></li>
+                                        <li><a href="/">LEGO® Animal Crossing™</a></li>
+                                        <li><a href="/">Minifiguras</a></li>
+                                        <li><a href="/">BrickHeadz</a></li>
+                                        <li><a href="/">LEGO® Art</a></li>
+                                        <li><a href="/">NINJAGO®</a></li>
+                                        <li><a href="/">City</a></li>
+                                        <li><a href="/">LEGO® Avatar</a></li>
+                                        <li><a href="/">Powered UP</a></li>
+                                        <li><a href="/">Classic</a></li>
+                                        <li><a href="/">LEGO® Braille Bricks</a></li>
+                                        <li><a href="/">SERIOUS PLAY®</a></li>
+                                        <li><a href="/">Creator 3in1</a></li>
+                                        <li><a href="/">LEGO® DREAMZzz™</a></li>
+                                        <li><a href="/">Sonic the Hedgehog™</a></li>
+                                        <li><a href="/">Creator Expert</a></li>
+                                        <li><a href="/">LEGO® DUPLO®</a></li>
+                                        <li><a href="/">Speed Champions</a></li>
+                                        <li><a href="/">DC</a></li>
+                                        <li><a href="/">LEGO® Education</a></li>
+                                        <li><a href="/">Spider-Man</a></li>
+                                        <li><a href="/">Gru: O Maldisposto 4</a></li>
+                                        <li><a href="/">LEGO® Casa das Bonecas de Gabby</a></li>
+                                        <li><a href="/">Star Wars™</a></li>
+                                        <li><a href="/">Disney</a></li>
+                                        <li><a href="/">LEGO® Icons</a></li>
+                                        <li><a href="/">Technic</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li><button type="button" onClick={handleAgeBtn}>Idade<img src="/images/chevron.svg" alt="" /></button></li>
+                            <li style={{display: ageDisplay}}><div className="regular-list styleAnch">
+                                    <ul>
+                                        <li className="regular-top-text"><a href="/">Ver todas as idades</a></li>
+                                        <li><a href="/">1,5+</a></li>
+                                        <li><a href="/">4+</a></li>
+                                        <li><a href="/">6+</a></li>
+                                        <li><a href="/">9+</a></li>
+                                        <li><a href="/">13+</a></li>
+                                        <li><a href="/">18+</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li><button type="button" onClick={handlePriceBtn}>Gama de preços<img src="/images/chevron.svg" alt="" /></button></li>
+                            <li style={{display: priceDisplay}}><div className="regular-list styleAnch">
+                                    <ul>
+                                        <li className="regular-top-text"><a href="/">Ver todas as gamas de preços</a></li>
+                                        <li><a href="/">Menos de 20€</a></li>
+                                        <li><a href="/">20€ a 50€</a></li>
+                                        <li><a href="/">50€ a 100€</a></li>
+                                        <li><a href="/">100€ a 200€</a></li>
+                                        <li><a href="/">Mais de 200€</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li><button type="button" onClick={handleProdBtn}>Produtos LEGO<img src="/images/chevron.svg" alt="" /></button></li>
+                            <li style={{display: prodDisplay}}><div className="regular-list styleAnch">
+                                    <ul>
+                                        <li className="regular-top-text"><a href="/">Ver todos os produtos LEGO</a></li>
+                                        <li><a href="/">Acessórios</a></li>
+                                        <li><a href="/">Mochilas</a></li>
+                                        <li><a href="/">Livros</a></li>
+                                        <li><a href="/">Roupa</a></li>
+                                        <li><a href="/">Porta-Chaves</a></li>
+                                        <li><a href="/">Lancheiras</a></li>
+                                        <li><a href="/">Brinquedos de Peluche</a></li>
+                                        <li><a href="/">Puzzles & Jogos de Tabuleiro</a></li>
+                                        <li><a href="/">Representações e Fantasia</a></li>
+                                        <li><a href="/">Papelaria</a></li>
+                                        <li><a href="/">Armazenamento</a></li>
+                                        <li><a href="/">Garrafas de Água LEGO®</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li><button type="button" onClick={handleIntBtn}>Interesses<img src="/images/chevron.svg" alt="" /></button></li>
+                            <li style={{display: intDisplay}}><div className="grid-list-2col styleAnch">
+                                    <div className="grid-list-top-text"><a href="/">Ver todos os produtos</a></div>
+                                    <ul>
+                                        <li><a href="/">Bem-vindos, adultos</a></li>
+                                        <li><a href="/">Sazonal</a></li>
+                                        <li><a href="/">Animais</a></li>
+                                        <li><a href="/">Espaço</a></li>
+                                        <li><a href="/">Edifícios</a></li>
+                                        <li><a href="/">Desporto</a></li>
+                                        <li><a href="/">Código para crianças</a></li>
+                                        <li><a href="/">STEM</a></li>
+                                        <li><a href="/">Dragões</a></li>
+                                        <li><a href="/">Comboios</a></li>
+                                        <li><a href="/">Fantasia</a></li>
+                                        <li><a href="/">Viagens</a></li>
+                                        <li><a href="/">Filmes</a></li>
+                                        <li><a href="/">Séries de TV</a></li>
+                                        <li><a href="/">Videojogos</a></li>
+                                        <li><a href="/">Veículos</a></li>
+                                        <li><a href="/">Aprender a construir</a></li>
+                                        <li><a href="/">Música</a></li>
+                                        <li><a href="/">Ninjas</a></li>
+                                        <li><a href="/">Pré-Escolar</a></li>
+                                        <li><a href="/">Princesas</a></li>
+                                        <li><a href="/">Heróis da Vida Real</a></li>
+                                        <li><a href="/">Robôs para Crianças</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li><button type="button" onClick={handlePabBtn}>Pick and Build<img src="/images/chevron.svg" alt="" /></button></li>
+                            <li style={{display: pabDisplay}}><div className="regular-list styleAnch">
+                                    <ul>
+                                        <li className="regular-top-text"><a href="/">Pick and Build</a></li>
+                                        <li><a href="/">Pick a Brick</a></li>
+                                        <li><a href="/">Kits e acessórios de construção</a></li>
+                                    </ul>
+                                </div>
+                            </li>
                         </ul>
                         <ul className="menu-list-links">
                             <li><a href="/">Exclusivos</a></li>
@@ -60,9 +237,30 @@ export default function Navbar() {
                     </div>
                     <div style={{display: content2}} className="menu-list">
                         <ul>
-                            <li><button type="button">Os nossos valores<img src="/images/chevron.svg" alt="" /></button></li>
-                            <li><button type="button">As nossas apps<img src="/images/chevron.svg" alt="" /></button></li>
-                            <li><button type="button">As nossas revistas<img src="/images/chevron.svg" alt="" /></button></li>
+                            <li><button type="button" onClick={handleValBtn}>Os nossos valores<img src="/images/chevron.svg" alt="" /></button></li>
+                            <li style={{display: valDisplay}}><div className="regular-list styleAnch">
+                                    <ul>
+                                        <li><a href="/">Sobre o LEGO Group</a></li>
+                                        <li><a href="/">Notícias LEGO</a></li>
+                                        <li><a href="/">Sustentabilidade</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li><button type="button" onClick={handleAppBtn}>As nossas apps<img src="/images/chevron.svg" alt="" /></button></li>
+                            <li style={{display: appDisplay}}><div className="regular-list styleAnch">
+                                    <ul>
+                                        <li><a href="/">LEGO Builder</a></li>
+                                        <li><a href="/">App LEGO Life</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li><button type="button" onClick={handleMagBtn}>As nossas revistas<img src="/images/chevron.svg" alt="" /></button></li>
+                            <li style={{display: magDisplay}}><div className="regular-list styleAnch">
+                                    <ul>
+                                        <li><a href="/">Catálogos LEGO</a></li>
+                                    </ul>
+                                </div>
+                            </li>
                         </ul>
                         <ul className="menu-list-links">
                             <li><a href="/">Todos os temas LEGO</a></li>
@@ -87,9 +285,12 @@ export default function Navbar() {
                             <li><a href="/">Peças de substituição</a></li>
                         </ul>
                     </div>
+                    <div className="submenu-list">
+                        
+                    </div>
                 </div>
-                <div className="menu-content" style={{display: menuDisplay}}></div>
-                <div className="menu-content-dark" style={{display: menuDisplay}}></div>
+                <div className="menu-content" style={{display: menuDisplay, maxWidth: contentMaxWidth}}></div>
+                <div className="menu-content-dark" style={{display: menuDisplay}} onClick={menuClose}></div>
             </div> 
 
         </nav>
